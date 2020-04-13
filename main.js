fbbfa8002db5f76d52554998315ab244caad56e6
cmchase4664 <carmine.chase@centurylink.com>
2020-04-13 01:49:39 +0000

    Lab 3

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ const path = require('https://6161-77042018.itential-academy-labs.appsembler.com
  * as this module. IAP requires the path object's join method
  * to unequivocally locate the file module.
  */
-const { getIpv4MappedIpv6Address } = require(/opt/pronghorn-workspace/app-ip_address);
+const { getIpv4MappedIpv6Address } = require(path.join(/opt/pronghorn-workspace/app-ip_address, 'ipv6.js'));
   // Use the object's isValid() method to verify the passed CIDR.
   if (!cidr.isValid()) {
     // If the passed CIDR is invalid, set an error message.
